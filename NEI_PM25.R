514577f3f98c5296f9cb09bc3536d6f98343445f
Hervinz <hszuluagas@unal.edu.co>
2020-09-10 08:25:49 -0500

    Analisys of Baltimore City and Maryland.

--- NEI_PM25.R
+++ NEI_PM25.R
@@ -26,7 +26,46 @@ yr2002 <- NEI[with(NEI, year == 2002),4]
 yr2005 <- NEI[with(NEI, year == 2005),4]
 yr2008 <- NEI[with(NEI, year == 2008),4]
 
+summary(yr1999)
+summary(yr2002)
+summary(yr2005)
+summary(yr2008)
 
+##Both the mean and the maximum value of the different samples
+#allow us to supous that there is a reduction in the total PM25 
+#particulate material in the course of 1999, 2002, 2005 and 2008, 
+#it is possible also be seen in the boxplot, especially in the logarithmic base 10 boxplot.
 
 boxplot(yr1999, yr2002, yr2005, yr2008)
+boxplot(log10(yr1999), log10(yr2002), log10(yr2005), log10(yr2008))
+
+
+
+##Have total emissions from PM2.5 decreased in the Baltimore City,
+#Maryland (fips == "24510" | fips == "24510") from 1999 to 2008?
+#Use the base plotting system to make a plot answering this question.
+
+BC_Mryld <- NEI[with(NEI, fips == "24510" | fips == "24510"),]
+BMyr1999 <- BC_Mryld[with(BC_Mryld, year == 1999),4]
+BMyr2002 <- BC_Mryld[with(BC_Mryld, year == 2002),4]
+BMyr2005 <- BC_Mryld[with(BC_Mryld, year == 2005),4]
+BMyr2008 <- BC_Mryld[with(BC_Mryld, year == 2008),4]
+
+summary(BMyr1999)
+summary(BMyr2002)
+summary(BMyr2005)
+summary(BMyr2008)
+
+##Both the mean and the maximum value of the different samples
+# don´t allow us to supous that there is a reduction in the total PM25 
+#particulate material in the course of 1999, 2002, 2005 and 2008-
+#It seems in year 2005 there wasn't a reduction with respect to year 2002.
+
+boxplot(BMyr1999, BMyr2002, BMyr2005, BMyr2008)
+boxplot(log10(BMyr1999), log10(BMyr2002), log10(BMyr2005), log10(BMyr2008))
+
+
+
+
+
 
